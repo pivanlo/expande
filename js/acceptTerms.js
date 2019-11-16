dff96e1d2a133c406ce5c5b5f77a09b463511438
Pedro Iván López <pivanlopez@gmail.com>
2019-11-16 22:18:23 +0100

    finish form validation

--- js/acceptTerms.js
+++ js/acceptTerms.js
@@ -27,12 +27,82 @@ function initAcceptTerms() {
 
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  var $name = $('#name');
+  var $email = $('#email');
+  var $message = $('#message');
+
+  // Hide all errors
+  $name.popover('hide');
+  $email.popover('hide');
+  $message.popover('hide');
+  $('#termsPopover').hide();
+
+  // Validate name
+  if (!$name.val()) {
+    $name.popover('show');
+    return;
+  } else {
+    $name.popover('hide');
+  }
+
+  // Validate email
+  var emailValue = $email.val();
+  if (!emailValue) {
+    $email.attr('data-content', '<i class="fas fa-exclamation"></i> Porfavor introduce tu correo electrónico.');
+    $email.popover('show');
+    return;
+  } else if (!validateEmail(emailValue)) {
+    $email.attr('data-content', '<i class="fas fa-exclamation"></i> El correo electrónico no tiene un formato válido.');
+    $email.popover('show');
+    return;
+  } else {
+    $email.popover('hide');
+  }
+
+  // Validate message
+  if (!$message.val()) {
+    $message.popover('show');
+    return;
+  } else {
+    $message.popover('hide');
+  }
+
+  // Validate terms
+  if (!$('#terms').is(":checked")) {
+    $('#termsPopover').show();
+    return;
+  }
+
+  console.log('sending!');
+}
+
+function handleInputKeyUp() {
+  if ($(this).val()) {
+    $(this).popover('hide');
+  }
+};
+
+function handleTermsChange() {
+  if ($('#terms').is(":checked")) {
+    $('#termsPopover').hide();
+    return;
+  }
+};
+
 
 $(document).ready(function(){
-  initAcceptTerms();
+  $('input').popover({trigger: 'manual'});
+  $('.contact-form').submit(handleSubmit);
+  $('#name, #email, #message').keyup(handleInputKeyUp);
+  $('#terms').change(handleTermsChange);
+  // initAcceptTerms();
 });
 
-
-$(document).ready(function(){
-  $('[data-toggle="popover"]').popover(); 
-});
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}

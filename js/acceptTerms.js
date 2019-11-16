79adea940934a9fec772dbb688247e197fcca8bc
Pedro Iván López <pivanlopez@gmail.com>
2019-11-16 09:51:34 +0100

    remove holder.js

--- js/acceptTerms.js
+++ js/acceptTerms.js
@@ -2,15 +2,29 @@
  * Enables the submit button when the accept terms checbox is checked.
  */
 function initAcceptTerms() {
-  $('#accept-terms').on('change', function(event) {
-    var checked = this.checked;
-    var $submitButton = $('.contact-form button[type="submit"]');
-    if (checked) {
-      $submitButton.removeAttr('disabled')
-    } else {
-      $submitButton.attr('disabled', true)
-    }
+  // $('#accept-terms').on('change', function(event) {
+  //   var checked = this.checked;
+  //   var $submitButton = $('.contact-form button[type="submit"]');
+  //   if (checked) {
+  //     $submitButton.removeAttr('disabled')
+  //   } else {
+  //     $submitButton.attr('disabled', true)
+  //   }
+  // });
+
+  var forms = document.getElementsByClassName('needs-validation');
+  // Loop over them and prevent submission
+  var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      // if (form.checkValidity() === false) {
+        console.log('here')
+        event.preventDefault();
+        event.stopPropagation();
+      // }
+      form.classList.add('was-validated');
+    }, false);
   });
+
 }
 
 
